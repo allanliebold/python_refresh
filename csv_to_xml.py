0fcbc1a26d7724cc35d50dd7239907c6069ee3b8
Allan Liebold <allan.liebold@gmail.com>
2018-11-03 12:17:20 -0700

    Update csv_to_xml.py

--- csv_to_xml.py
+++ csv_to_xml.py
@@ -4,6 +4,8 @@ from xml.etree import ElementTree
 def main(file_path):
   tree = ElementTree.parse(file_path)
   root = tree.getroot()
+  
+  New_CSV = csv.writer('new_csv.csv', 'w')
 
 if __name__ == '__main__':
   main(sys.argv[1])  

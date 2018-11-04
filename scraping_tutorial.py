ca675b2ed857901e1e5ea5aedd72170ef999a48e
Allan Liebold <allan.liebold@gmail.com>
2018-11-04 08:04:34 -0800

    Update scraping_tutorial.py

--- scraping_tutorial.py
+++ scraping_tutorial.py
@@ -4,5 +4,4 @@ from bs4 import BeautifulSoup
 page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
 soup = BeautifulSoup(page.content, "html.parser")
 
-print(page.statuscode) 
-print(page.content)
+seven_day = soup.find(id="seven-day-forecast")

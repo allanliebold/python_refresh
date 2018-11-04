0877aec5ba0e51c00ca8736b57e27d85687774f2
Allan Liebold <allan.liebold@gmail.com>
2018-11-04 09:22:10 -0800

    Update gar_scrape.py

--- gar_scrape.py
+++ gar_scrape.py
@@ -5,5 +5,9 @@ from bs4 import BeautifulSoup
 page = requests.get("http://www.gartran.com/main.php/current_loads?table_view=1&page_order=id&page_dir=xxx&page_num=1")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-order_numbers = [order.get_text() for order in soup.find_all(class_="tablelink")]
-print(order_numbers)
+truck_types = []
+origin_states = []
+origin_cities = []
+dest_states = []
+dest_cities = []
+miles = []

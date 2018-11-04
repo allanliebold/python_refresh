9305b3b983d32beb93304a7825fd1b83d1bca98a
Allan Liebold <allan.liebold@gmail.com>
2018-11-04 08:02:19 -0800

    Update scraping_tutorial.py

--- scraping_tutorial.py
+++ scraping_tutorial.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
 print(page.statuscode) 
 print(page.content)

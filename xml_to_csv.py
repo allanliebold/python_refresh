07c5d322a033751d4c868fb331a57a27b3f16d0b
Allan Liebold <allan.liebold@gmail.com>
2018-11-05 06:21:17 -0800

    Update xml_to_csv.py

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -1,1 +1,15 @@
+"""XML to CSV Scraper."""
+import sys, csv
+from xml.etree import ElementTree
+
+def main(file_path):
+  tree = ElementTree.parse(file_path)
+  root = tree.getroot()
+  
+  New_CSV = csv.writer('new_csv.csv', 'w')
+  
+  New_CSV.close()
+
+if __name__ == '__main__':
+  main(sys.argv[1])  
 

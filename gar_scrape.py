95736cdc68702be71f2217c2944276f7fca5a2fb
Allan Liebold <allan.liebold@gmail.com>
2018-11-05 06:03:38 -0800

    Update gar_scrape.py

--- gar_scrape.py
+++ gar_scrape.py
@@ -26,6 +26,7 @@ def write_csv():
                                                       'destination_city', 'destination_state', 'destination_zip',
                                                       'truck_type', 'distance', 'pick_date', 'pick_time', 'drop_date',
                                                       'drop_time', 'stops', 'shipper'])
+        
         writer.writeheader()
         for i in range(0, len(order_numbers)):
             writer.writerow({'reference_id':order_numbers[i], 'origin_city':origin_cities[i], 'origin_state':origin_states[i],

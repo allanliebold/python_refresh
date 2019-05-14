7a61538491899abf52167fc0121bc5d884d6ea16
Allan Liebold <allan.liebold@gmail.com>
2019-05-14 06:18:10 -0700

    Update xml_to_csv.py

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -5,7 +5,6 @@ from xml.etree import ElementTree
 def main(file_path):
   tree = ElementTree.parse(file_path)
   root = tree.getroot()
-  
   New_CSV = csv.writer('new_csv.csv', 'w')
   New_CSV.close()
 
